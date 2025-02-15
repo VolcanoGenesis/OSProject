62c8862b71479098ff214837a211105777c0fd58
VolcanoGenesis <118592623+VolcanoGenesis@users.noreply.github.com>
2025-02-16 02:09:06 +0530

    Update moderator.c

--- OS Project/moderator.c
+++ OS Project/moderator.c
@@ -1,121 +1,192 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define MAX_FILTERED_WORDS 50
 #define MAX_TEXT_SIZE 256
 #define MAX_GROUPS 30
 #define MAX_USERS 50
+#define MAX_WORD_LENGTH 20
 
 typedef struct {
-    long mtype;
-    int timestamp;
-    int user;
-    char mtext[MAX_TEXT_SIZE];
-    int modifyingGroup;
+	long mtype;
+	int timestamp;
+	int user;
+	char mtext[MAX_TEXT_SIZE];
+	int modifyingGroup;
 } Message;
 
-void loadFilteredWords(const char *filePath, char filteredWords[MAX_FILTERED_WORDS][MAX_TEXT_SIZE], int *wordCount) {
-    FILE *file = fopen(filePath, "r");
-    if (!file) {
-        perror("Failed to open filtered words file");
-        exit(EXIT_FAILURE);
-    }
+int moderatorQueue = -1;
+char filteredWords[MAX_FILTERED_WORDS][MAX_WORD_LENGTH];
+int wordCount = 0;
+int violations[MAX_GROUPS][MAX_USERS] = {0};
+int bannedUsers[MAX_GROUPS] = {0};
+int messagesReceived = 0;
+int groupsDeleted = 0;
 
-    *wordCount = 0;
-    while (fgets(filteredWords[*wordCount], MAX_TEXT_SIZE, file) && *wordCount < MAX_FILTERED_WORDS) {
-        // Remove newline character if present
-        filteredWords[*wordCount][strcspn(filteredWords[*wordCount], "\n")] = 0;
-        (*wordCount)++;
-    }
-
-    fclose(file);
+void cleanup() {
+	if (moderatorQueue != -1) {
+    	msgctl(moderatorQueue, IPC_RMID, NULL);
+	}
 }
 
-int countViolations(const char *message, char filteredWords[MAX_FILTERED_WORDS][MAX_TEXT_SIZE], int wordCount) {
-    int violations = 0;
-    char lowercaseMessage[MAX_TEXT_SIZE];
-    strcpy(lowercaseMessage, message);
+void signal_handler(int signum) {
+	cleanup();
+	exit(EXIT_FAILURE);
+}
 
-    // Convert message to lowercase
-    for (int i = 0; lowercaseMessage[i]; i++) {
-        lowercaseMessage[i] = tolower(lowercaseMessage[i]);
-    }
+void loadFilteredWords(const char *filePath) {
+	FILE *file = fopen(filePath, "r");
+	if (!file) {
+    	perror("Failed to open filtered words file");
+    	exit(EXIT_FAILURE);
+	}
 
-    // Check for each filtered word
-    for (int i = 0; i < wordCount; i++) {
-        char lowercaseFilteredWord[MAX_TEXT_SIZE];
-        strcpy(lowercaseFilteredWord, filteredWords[i]);
-        for (int j = 0; lowercaseFilteredWord[j]; j++) {
-            lowercaseFilteredWord[j] = tolower(lowercaseFilteredWord[j]);
-        }
+	char line[MAX_WORD_LENGTH];
+	while (fgets(line, sizeof(line), file) && wordCount < MAX_FILTERED_WORDS) {
+    	size_t len = strlen(line);
+    	if (len > 0 && line[len-1] == '\n') {
+        	line[len-1] = '\0';
+    	}
+    	if (strlen(line) > 0) {
+        	strncpy(filteredWords[wordCount], line, MAX_WORD_LENGTH - 1);
+        	filteredWords[wordCount][MAX_WORD_LENGTH - 1] = '\0';
+        	wordCount++;
+    	}
+	}
+	fclose(file);
+}
 
-        if (strstr(lowercaseMessage, lowercaseFilteredWord) != NULL) {
-            violations++;
-        }
-    }
+int countViolations(const char *message) {
+	int uniqueViolations = 0;
+	int foundWords[MAX_FILTERED_WORDS] = {0};
+    
+	char lowercaseMessage[MAX_TEXT_SIZE];
+	strncpy(lowercaseMessage, message, MAX_TEXT_SIZE - 1);
+	lowercaseMessage[MAX_TEXT_SIZE - 1] = '\0';
+    
+	for (char *p = lowercaseMessage; *p; p++) {
+    	*p = tolower(*p);
+	}
 
-    return violations;
+	for (int i = 0; i < wordCount; i++) {
+    	if (foundWords[i]) continue;
+
+    	char lowercaseFilter[MAX_WORD_LENGTH];
+    	strncpy(lowercaseFilter, filteredWords[i], MAX_WORD_LENGTH - 1);
+    	lowercaseFilter[MAX_WORD_LENGTH - 1] = '\0';
+   	 
+    	for (char *p = lowercaseFilter; *p; p++) {
+        	*p = tolower(*p);
+    	}
+
+    	char *pos = lowercaseMessage;
+    	while ((pos = strstr(pos, lowercaseFilter)) != NULL) {
+        	int isWordStart = (pos == lowercaseMessage || !isalnum(*(pos - 1)));
+        	int isWordEnd = (!*(pos + strlen(lowercaseFilter)) ||
+                       	!isalnum(*(pos + strlen(lowercaseFilter))));
+       	 
+        	if (isWordStart && isWordEnd) {
+            	foundWords[i] = 1;
+            	uniqueViolations++;
+            	break;
+        	}
+        	pos++;
+    	}
+	}
+
+	return uniqueViolations;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <test_case_number>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2) {
+    	fprintf(stderr, "Usage: %s <test_case_number>\n", argv[0]);
+    	exit(EXIT_FAILURE);
+	}
 
-    int testCaseNumber = atoi(argv[1]);
-    char inputFilePath[256];
-    snprintf(inputFilePath, sizeof(inputFilePath), "testcase_%d/input.txt", testCaseNumber);
+	signal(SIGINT, signal_handler);
+	signal(SIGTERM, signal_handler);
 
-    FILE *inputFile = fopen(inputFilePath, "r");
-    if (!inputFile) {
-        perror("Failed to open input file");
-        exit(EXIT_FAILURE);
-    }
+	int testCaseNumber = atoi(argv[1]);
+    
+	char inputFilePath[256];
+	snprintf(inputFilePath, sizeof(inputFilePath), "testcase_%d/input.txt", testCaseNumber);
+    
+	FILE *inputFile = fopen(inputFilePath, "r");
+	if (!inputFile) {
+    	perror("Failed to open input file");
+    	exit(EXIT_FAILURE);
+	}
 
-    int numGroups, validationKey, appKey, moderatorKey, violationThreshold;
-    fscanf(inputFile, "%d %d %d %d %d", &numGroups, &validationKey, &appKey, &moderatorKey, &violationThreshold);
-    fclose(inputFile);
+	int numGroups, validationKey, appKey, moderatorKey, violationThreshold;
+	if (fscanf(inputFile, "%d %d %d %d %d", &numGroups, &validationKey, &appKey,
+           	&moderatorKey, &violationThreshold) != 5) {
+    	fprintf(stderr, "Failed to read configuration\n");
+    	fclose(inputFile);
+    	exit(EXIT_FAILURE);
+	}
+	fclose(inputFile);
 
-    char filteredWordsPath[256];
-    snprintf(filteredWordsPath, sizeof(filteredWordsPath), "testcase_%d/filtered_words.txt", testCaseNumber);
+	char filteredWordsPath[256];
+	snprintf(filteredWordsPath, sizeof(filteredWordsPath),
+         	"testcase_%d/filtered_words.txt", testCaseNumber);
+	loadFilteredWords(filteredWordsPath);
 
-    char filteredWords[MAX_FILTERED_WORDS][MAX_TEXT_SIZE];
-    int wordCount = 0;
-    loadFilteredWords(filteredWordsPath, filteredWords, &wordCount);
+	moderatorQueue = msgget(moderatorKey, IPC_CREAT | 0666);
+	if (moderatorQueue == -1) {
+    	perror("Failed to create moderator message queue");
+    	exit(EXIT_FAILURE);
+	}
 
-    int moderatorQueue = msgget(moderatorKey, IPC_CREAT | 0666);
-    if (moderatorQueue == -1) {
-        perror("Failed to create moderator message queue");
-        exit(EXIT_FAILURE);
-    }
+	Message msg;
+	int activeGroups = numGroups;
 
-    int violations[MAX_GROUPS][MAX_USERS] = {0};
-    Message msg;
+	while (activeGroups > 0) {
+    	if (msgrcv(moderatorQueue, &msg, sizeof(msg) - sizeof(msg.mtype), 1, 0) == -1) {
+        	if (errno == EINTR) continue;
+        	if (errno == EIDRM) {
+            	printf("msgrcv failed: Identifier removed\n");
+            	break;
+        	}
+        	perror("Message receive error");
+        	break;
+    	}
 
-    while (1) {
-        if (msgrcv(moderatorQueue, &msg, sizeof(msg) - sizeof(msg.mtype), 1, 0) > 0) {
-            int violationCount = countViolations(msg.mtext, filteredWords, wordCount);
-            violations[msg.modifyingGroup][msg.user] += violationCount;
+    	messagesReceived++;
+   	 
+    	int newViolations = countViolations(msg.mtext);
+    	if (newViolations > 0) {
+        	violations[msg.modifyingGroup][msg.user] += newViolations;
+       	 
+        	if (violations[msg.modifyingGroup][msg.user] >= violationThreshold &&
+            	bannedUsers[msg.modifyingGroup] < MAX_USERS) {
+           	 
+            	Message banMsg = {
+                	.mtype = 2,
+                	.user = msg.user,
+                	.modifyingGroup = msg.modifyingGroup
+            	};
+            	if (msgsnd(moderatorQueue, &banMsg, sizeof(Message) - sizeof(long), 0) == -1) {
+                	if (errno == EIDRM) {
+                    	printf("msgsnd failed: Identifier removed\n");
+                    	break;
+                	}
+            	}
+            	bannedUsers[msg.modifyingGroup]++;
+        	}
+    	}
+	}
 
-            if (violations[msg.modifyingGroup][msg.user] >= violationThreshold) {
-                printf("User %d of Group %d banned after %d violations\n", 
-                       msg.user, msg.modifyingGroup, violations[msg.modifyingGroup][msg.user]);
-
-                // Send ban message to group
-                Message banMsg;
-                banMsg.mtype = 2; // Ban message type
-                banMsg.user = msg.user;
-                banMsg.modifyingGroup = msg.modifyingGroup;
-                msgsnd(moderatorQueue, &banMsg, sizeof(banMsg) - sizeof(banMsg.mtype), 0);
-            }
-        }
-    }
-
-    return 0;
+	cleanup();
+	return 0;
 }
+
+
+
 
