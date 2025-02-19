d2499e49f5c2c1ec7ce070fc7bb9ddf85d5d2d37
VolcanoGenesis <118592623+VolcanoGenesis@users.noreply.github.com>
2025-02-19 19:33:15 +0530

    Update app.c

--- OS Project/app.c
+++ OS Project/app.c
@@ -1,153 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
 #define MAX_GROUPS 30
-#define MAX_PATH_LENGTH 256
-#define MAX_TEXT_SIZE 256
+#define MAX_MESSAGE_LENGTH 256
 
 typedef struct {
-	long mtype;
-	int timestamp;
-	int user;
-	char mtext[MAX_TEXT_SIZE];
-	int modifyingGroup;
+    long mtype;
+    int group;
 } Message;
 
-int appMsgQueue = -1;
-pid_t child_pids[MAX_GROUPS];
-int num_children = 0;
-int totalUsers = 0;
-int totalBannedUsers = 0;
-int groupsCreated = 0;
-int messagesReceived = 0;
-
-void cleanup() {
-	if (appMsgQueue != -1) {
-    	msgctl(appMsgQueue, IPC_RMID, NULL);
-	}
-    
-	for (int i = 0; i < num_children; i++) {
-    	if (child_pids[i] > 0) {
-        	kill(child_pids[i], SIGTERM);
-        	waitpid(child_pids[i], NULL, 0);
-    	}
-	}
-}
-
-void signal_handler(int signum) {
-	cleanup();
-	exit(EXIT_FAILURE);
-}
+typedef struct {
+    long mtype;
+    int user;
+    char mtext[MAX_MESSAGE_LENGTH];
+    int violations;
+    int grp_id;
+    int action;
+    int terminator;
+} ModeratorMessage;
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-    	fprintf(stderr, "Usage: %s <test_case_number>\n", argv[0]);
-    	exit(EXIT_FAILURE);
-	}
-
-	signal(SIGINT, signal_handler);
-	signal(SIGTERM, signal_handler);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <test_case_number>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
     
-	int testCaseNumber = atoi(argv[1]);
-	char inputFilePath[MAX_PATH_LENGTH];
-	snprintf(inputFilePath, sizeof(inputFilePath), "testcase_%d/input.txt", testCaseNumber);
-
-	FILE *inputFile = fopen(inputFilePath, "r");
-	if (!inputFile) {
-    	perror("Failed to open input.txt");
-    	exit(EXIT_FAILURE);
-	}
-
-	int n, validationKey, appGroupsKey, moderatorGroupsKey, violationThreshold;
-	if (fscanf(inputFile, "%d %d %d %d %d", &n, &validationKey, &appGroupsKey,
-           	&moderatorGroupsKey, &violationThreshold) != 5) {
-    	fprintf(stderr, "Failed to read configuration\n");
-    	fclose(inputFile);
-    	exit(EXIT_FAILURE);
-	}
-
-	char groupPaths[MAX_GROUPS][MAX_PATH_LENGTH];
-	for (int i = 0; i < n; i++) {
-    	if (fscanf(inputFile, "%s", groupPaths[i]) != 1) {
-        	fprintf(stderr, "Failed to read group path %d\n", i);
-        	fclose(inputFile);
-        	exit(EXIT_FAILURE);
-    	}
-	}
-	fclose(inputFile);
-
-	appMsgQueue = msgget(appGroupsKey, IPC_CREAT | 0666);
-	if (appMsgQueue == -1) {
-    	perror("Failed to create app message queue");
-    	exit(EXIT_FAILURE);
-	}
-
-	for (int i = 0; i < n; i++) {
-    	pid_t pid = fork();
-    	if (pid == 0) {
-        	char fullGroupPath[MAX_PATH_LENGTH];
-        	snprintf(fullGroupPath, sizeof(fullGroupPath), "testcase_%d/%s",
-                	testCaseNumber, groupPaths[i]);
-       	 
-        	char validationKeyStr[20], appGroupsKeyStr[20], moderatorGroupsKeyStr[20];
-        	sprintf(validationKeyStr, "%d", validationKey);
-        	sprintf(appGroupsKeyStr, "%d", appGroupsKey);
-        	sprintf(moderatorGroupsKeyStr, "%d", moderatorGroupsKey);
-       	 
-        	execl("./groups.out", "./groups.out", fullGroupPath, validationKeyStr,
-              	appGroupsKeyStr, moderatorGroupsKeyStr, NULL);
-       	 
-        	perror("Failed to exec groups.out");
-        	exit(EXIT_FAILURE);
-    	} else if (pid < 0) {
-        	perror("Failed to fork group process");
-        	cleanup();
-        	exit(EXIT_FAILURE);
-    	} else {
-        	child_pids[num_children++] = pid;
-        	groupsCreated++;
-    	}
-	}
-
-	Message msg;
-	int activeGroups = n;
-
-	while (activeGroups > 0) {
-    	if (msgrcv(appMsgQueue, &msg, sizeof(msg) - sizeof(msg.mtype), 1, 0) > 0) {
-        	totalBannedUsers += msg.user;
-        	activeGroups--;
-        	messagesReceived++;
-    	} else if (errno == EIDRM) {
-        	printf("msgrcv failed: Identifier removed\n");
-        	break;
-    	}
-	}
-
-	// Wait for all child processes
-	for (int i = 0; i < num_children; i++) {
-    	int status;
-    	waitpid(child_pids[i], &status, 0);
-	}
-
-	printf("==========================================\n");
-	printf("Number of groups created: %d\n", groupsCreated);
-	printf("Number of users created: %d\n", totalUsers);
-	printf("Number of messages received: %d\n", messagesReceived);
-	printf("Number of users deleted: %d\n", totalBannedUsers);
-	printf("Number of groups deleted: %d\n", groupsCreated);
-	printf("==========================================\n");
-
-	cleanup();
-	return 0;
+    // Construct path to input.txt inside the testcase folder.
+    char input_file[256];
+    snprintf(input_file, sizeof(input_file), "testcase_%s/input.txt", argv[1]);
+    
+    FILE *file = fopen(input_file, "r");
+    if (!file) {
+        perror("Error opening input.txt");
+        return EXIT_FAILURE;
+    }
+    
+    int num_groups;
+    int val_msgq_key;
+    int app_msgq_key;
+    int mod_msgq_key;
+    int violation_threshold;
+    
+    if(fscanf(file, "%d %d %d %d %d", &num_groups, &val_msgq_key, &app_msgq_key, &mod_msgq_key, &violation_threshold) != 5){
+        fprintf(stderr, "Error reading input.txt\n");
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+    
+    // Read the group file paths.
+    char group_files[MAX_GROUPS][256];
+    for (int i = 0; i < num_groups; i++) {
+        fscanf(file, "%s", group_files[i]);
+    }
+    fclose(file);
+    
+    printf("App: Using message queue key: %d (Group ↔ App)\n", app_msgq_key);
+    
+    // Create (or connect to) the message queue for group termination (communication with app)
+    int msgq_id = msgget(app_msgq_key, IPC_CREAT | 0666);
+    if (msgq_id == -1) {
+        perror("msgget failed");
+        return EXIT_FAILURE;
+    }
+    
+    // For each group, construct its full file path (prepend the testcase folder)
+    pid_t pids[MAX_GROUPS];
+    for (int i = 0; i < num_groups; i++) {
+        char full_path[512]; // Increased size to avoid truncation
+        snprintf(full_path, sizeof(full_path), "testcase_%s/%s", argv[1], group_files[i]);
+        
+        if ((pids[i] = fork()) == 0) {
+            printf("Executing: ./group.out %s %s\n", argv[1], full_path);
+            execl("./group.out", "group.out", argv[1], full_path, NULL);
+            perror("execl failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    // Wait for group termination messages
+    int terminated_groups = 0;
+    Message msg;
+   
+    while (terminated_groups < num_groups) {
+        if (msgrcv(msgq_id, &msg, sizeof(Message) - sizeof(long), 0, 0) > 0) {
+            printf("All users terminated. Exiting group process %d.\n", msg.group);
+            terminated_groups++;
+        }
+    }
+    
+    
+    // Cleanup the app↔group message queue
+    msgctl(msgq_id, IPC_RMID, NULL);
+    return EXIT_SUCCESS;
 }
-
-
-
-
